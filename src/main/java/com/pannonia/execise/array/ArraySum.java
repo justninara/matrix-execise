349a5bf5d8c2c24acfb29391032ed8e2b1ac8cc2
ninad <nikolina.damjanovic@pannonia-expertise.net>
2020-06-26 17:44:45 +0200

    Tasks 1 - array sum

--- src/main/java/com/pannonia/execise/array/ArraySum.java
+++ src/main/java/com/pannonia/execise/array/ArraySum.java
@@ -3,6 +3,21 @@ package com.pannonia.execise.array;
 public class ArraySum {
 
   public Integer sum(Integer[] array) {
-    return null;// TODO implement sum calculation
+    Integer sum = 0;
+    if (array == null) {
+      throw new IllegalArgumentException("Array cannot be null");
+    }
+    for (Integer i : array) {
+      if (i == null) {
+        throw new IllegalArgumentException("Invalid input. Array element cannot be null");
+      }
+      Integer sumTmp = sum;
+      sum += i;
+      if (sum < sumTmp) {
+        throw new IllegalArgumentException("Invalid input. Sum of array elements can't be bigger than MAX integer value.");
+      }
+    }
+    return sum;
+
   }
 }
--- src/test/java/com/pannonia/execise/array/ArraySumTest.java
+++ src/test/java/com/pannonia/execise/array/ArraySumTest.java
@@ -1,6 +1,8 @@
 package com.pannonia.execise.array;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.shouldHaveThrown;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,13 +20,28 @@ public class ArraySumTest {
     arraySum = new ArraySum();
   }
 
+  @Test
+  public void testSumIllegalArgumentException1() {
+
+    // GIVEN
+    Integer[] numbers =  new Integer[] { 1, 2, null, 4, 5 };
+
+    // WHEN
+    Throwable ex = catchThrowable(() -> arraySum.sum(numbers));
+
+    // THEN
+    assertThat(ex)//
+        .isInstanceOf(IllegalArgumentException.class)//
+        .hasMessage("Invalid input. Array element cannot be null");
+  }
+
   /**
    * <b>GIVEN</b> array of integer values</br>
    * <b>WHEN</b> sum is calculated</br>
    * <b>THEN</b> sum value is as expected</br>
    */
   @Test
-  @Disabled // TODO remove @Disabled annotation in order to enable the test
+//  @Disabled // TODO remove @Disabled annotation in order to enable the test
   public void testSum() {
 
     // GIVEN
@@ -36,6 +53,70 @@ public class ArraySumTest {
     // THEN
     assertThat(sum).isEqualTo(15);
   }
+
+  @Test
+  public void testSum1() {
+
+    // GIVEN
+    Integer[] numbers = new Integer[]{};
+
+    // WHEN
+    Integer sum = arraySum.sum(numbers);
+
+    // THEN
+    assertThat(sum).isEqualTo(0);
+  }
+  @Test
+  public void testSumIllegalArgumentException() {
+
+    // GIVEN
+    Integer[] numbers = null;
+
+    // WHEN
+    Throwable ex = catchThrowable(() -> arraySum.sum(numbers));
+
+    // THEN
+    assertThat(ex)//
+     .isInstanceOf(IllegalArgumentException.class)//
+    .hasMessage("Array cannot be null");
+  }
+
+  @Test
+  public void testSumElementIllegalArgumentException() {
+
+    // GIVEN
+    Integer[] numbers =  new Integer[] { 1, null, 3, 4, 5 };
+
+    // WHEN
+    Throwable ex = catchThrowable(() -> arraySum.sum(numbers));
+
+    // THEN
+    assertThat(ex)//
+        .isInstanceOf(IllegalArgumentException.class)//
+        .hasMessage("Invalid input. Array element cannot be null");
+  }
+
+  @Test
+  public void testSumMaxValue() {
+
+    // GIVEN
+    Integer[] numbers =  new Integer[] {Integer.MAX_VALUE, 1, 3, 4, 5 };
+
+    // WHEN
+    Throwable ex = catchThrowable(() -> arraySum.sum(numbers));
+
+    // THEN
+    assertThat(ex)//
+        .isInstanceOf(IllegalArgumentException.class)//
+        .hasMessage("Invalid input. Sum of array elements can't be bigger than MAX integer value.");
+  }
+
+
+
+
+
+
+
 
   @AfterEach
   public void afterEach() {
